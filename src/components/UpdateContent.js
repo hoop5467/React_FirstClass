574b5609c1ffdb8997c5fcab46000ae52fa2c5dc
hoop5467 <hoop5467@naver.com>
2021-03-04 17:31:25 +0900

    리액트 기본 강의

---
+++ src/components/UpdateContent.js
@@ -0,0 +1,56 @@
+import {Component} from "react";
+
+class UpdateContent extends Component {
+    constructor(props) {
+        super(props);
+        this.state ={
+            id : this.props.data.id,
+            title:this.props.data.title,
+            desc:this.props.data.desc
+        }
+        this.inputFormHandler = this.inputFormHandler.bind(this);
+    }
+    inputFormHandler(e){
+        this.setState(
+            {[e.target.name]: e.target.value})
+    }
+    render(){
+        return(
+            <article>
+                <h2>Update</h2>
+                <form action={"/Update_process"} method = "post"
+                      onSubmit={function (e){
+                          e.preventDefault();
+
+                          this.props.onSubmit(
+                              this.state.id,
+                              this.state.title,
+                              this.state.desc);
+
+                      }.bind(this)}
+                >
+                    <p>
+                        <input
+                        type="text"
+                        name={"title"}
+                        placeholder={"title"}
+                        value={this.state.title}
+                        onChange={this.inputFormHandler}
+                        />
+
+                    </p>
+                    <input type={"hidden"} name={"id"} value={this.state.id}/>
+                    <p>
+                        <textarea
+                            name={"desc"}
+                            placeholder={"description"}
+                            onChange={this.inputFormHandler}
+                        >{this.state.desc}</textarea>
+                    </p>
+                    <p><input type={"submit"}/></p>
+                </form>
+            </article>
+        )
+    }
+}
+export default UpdateContent;
